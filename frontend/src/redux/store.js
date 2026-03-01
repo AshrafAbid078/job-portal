764d35ec36d8543eae8d4b57082e964a1bc137d1
AshrafAbid078 <ashraf.abid0849@gmail.com>
2026-03-01 08:38:25 +0600

    Fix import case for jobSlice in store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-import jobSlice from "./jobSlice";
+import jobSlice from "./JobSlice";
 import companySlice from "./companySlice";
 import applicationSlice from "./applicationSlice";
 import {

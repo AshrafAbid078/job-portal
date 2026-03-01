bdbbbec8a0336e83cd1e4ceb4c1802275fd43d18
AshrafAbid078 <ashraf.abid0849@gmail.com>
2026-03-01 08:11:48 +0600

    2nd time
    2nd commit

--- frontend/src/Utils/constant.js
+++ frontend/src/Utils/constant.js
@@ -1,6 +1,13 @@
-const USERAPI_END_POINT = "http://localhost:5000/api/v1/users";
-const JOBAPI_END_POINT = "http://localhost:5000/api/v1/jobs";
-const APPLICANT_API_END_POINT = "http://localhost:5000/api/v1/applications";
-const COMPANY_API_END_POINT = "http://localhost:5000/api/v1/companies";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-export { USERAPI_END_POINT, JOBAPI_END_POINT, APPLICANT_API_END_POINT, COMPANY_API_END_POINT };
+const USERAPI_END_POINT = `${BASE_URL}/api/v1/users`;
+const JOBAPI_END_POINT = `${BASE_URL}/api/v1/jobs`;
+const APPLICANT_API_END_POINT = `${BASE_URL}/api/v1/applications`;
+const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/companies`;
+
+export {
+  USERAPI_END_POINT,
+  JOBAPI_END_POINT,
+  APPLICANT_API_END_POINT,
+  COMPANY_API_END_POINT
+};
